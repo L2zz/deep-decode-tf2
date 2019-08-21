1935366c2eb8d8b1edbfd085ac6497d9a0742c37
L2zz <ulla4571@gmail.com>
2019-08-22 02:25:18 +0900

    Implement ceiling function

--- signal_prep.py
+++ signal_prep.py
@@ -12,6 +12,7 @@ class Signal:
 
     def __init__(self, fn, in_data, idx):
         self.values = [float(s) for s in in_data[START:END]]
+        self.values = ceiling(self.values, np.mean(self.values)*2)
         self.values = MinMaxScaler(self.values)
         self.values = self.values - np.mean(self.values)
 
@@ -24,6 +25,13 @@ def MinMaxScaler(data):
     denominator = np.max(data, 0) - np.min(data, 0)
     # noise term prevents the zero division
     return numerator / (denominator + 1e-7)
+
+def ceiling(data, ceiling_value):
+    for i in range(len(data)):
+        if data[i] > ceiling_value:
+            data[i] = ceiling_value
+
+    return data
 
 # Global variables for determining start/end point of singal
 START = 100 + 1

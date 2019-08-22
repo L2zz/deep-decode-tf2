2cd892bd3f786ddef73b40524835048679ad45c5
L2zz <ulla4571@gmail.com>
2019-08-22 18:30:31 +0900

    Set start point of plotting

--- plot_graph.py
+++ plot_graph.py
@@ -6,9 +6,10 @@ import sys
 if __name__ == "__main__":
     FILE_NAME = [sys.argv[1]]
     MAX_NUM_SIG = int(sys.argv[2])
+    START = int(sys.argv[3])
     data_arr = read_file(FILE_NAME, MAX_NUM_SIG)
 
     for fn, data in data_arr.items():
-        for signal in data:
-            plt.plot(signal.values)
+        for i in range(START, len(data)):
+            plt.plot(data[i].values)
             plt.show()

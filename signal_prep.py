3ec05764e4a034391a37c7bd03d6ed3d78fd34a7
L2zz <ulla4571@gmail.com>
2019-08-22 18:30:18 +0900

    Modify preprocessing
    clipping out of (2.5%, 97.5%)

--- signal_prep.py
+++ signal_prep.py
@@ -4,6 +4,11 @@ module for defining signal and preprocessing
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Global variables for determining start/end point of singal
+START = 100 + 1
+END = -400
+
+
 class Signal:
     values = []  # sample values
     fn = ""  # from filename
@@ -12,7 +17,8 @@ class Signal:
 
     def __init__(self, fn, in_data, idx):
         self.values = [float(s) for s in in_data[START:END]]
-        self.values = ceiling(self.values, np.mean(self.values)*2)
+        self.values = ceiling(self.values, np.percentile(self.values, 2.5),
+                              np.percentile(self.values, 97.5))
         self.values = MinMaxScaler(self.values)
         self.values = self.values - np.mean(self.values)
 
@@ -20,19 +26,19 @@ class Signal:
         self.epc = [int(bit) for bit in list(in_data[0])]
         self.data_idx = idx
 
+
 def MinMaxScaler(data):
     numerator = data - np.min(data, 0)
     denominator = np.max(data, 0) - np.min(data, 0)
     # noise term prevents the zero division
     return numerator / (denominator + 1e-7)
 
-def ceiling(data, ceiling_value):
+
+def ceiling(data, min, max):
     for i in range(len(data)):
-        if data[i] > ceiling_value:
-            data[i] = ceiling_value
+        if data[i] > max:
+            data[i] = max
+        elif data[i] < min:
+            data[i] = min
 
     return data
-
-# Global variables for determining start/end point of singal
-START = 100 + 1
-END = -400
